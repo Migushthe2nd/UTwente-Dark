bb7599afec17d44b3df506cc33d9c4b75eaf051e
Migushthe2nd <ihaverkamp@live.nl>
2020-11-06 01:12:28 +0100

    Styles finished, extension needs cleaning up

--- Extension/background.js
+++ Extension/background.js
@@ -1,6 +1,97 @@
-function isCSPHeader(headerName) {
+// Clear storage in dev environment
+// chrome.storage.sync.clear(function () {
+// 	var error = chrome.runtime.lastError;
+// 	if (error) {
+// 		console.error(error);
+// 	}
+// });
+
+chrome.runtime.onInstalled.addListener(() => {
+	chrome.declarativeContent.onPageChanged.removeRules(undefined, () => {
+		chrome.declarativeContent.onPageChanged.addRules([
+			{
+				conditions: [
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: {
+							hostSuffix: 'utwente.nl',
+						},
+					}),
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: {
+							hostSuffix: 'reu1.blindsidenetworks.com',
+						},
+					}),
+					new chrome.declarativeContent.PageStateMatcher({
+						pageUrl: {
+							hostSuffix: 'horusapp.nl',
+						},
+					}),
+				],
+				actions: [new chrome.declarativeContent.ShowPageAction()],
+			},
+		]);
+	});
+});
+
+chrome.pageAction.onClicked.addListener((tab) => {
+	const host = getHost(tab);
+	chrome.storage.sync.get({ disabled: {} }, (data) => {
+		data.disabled[host] = !Boolean(data.disabled[host]);
+
+		if (data.disabled[host]) {
+			setIcon(tab.id, false);
+		} else {
+			setIcon(tab.id, true);
+		}
+
+		// Persistant pageAction state: https://stackoverflow.com/a/31807498/12314121
+		chrome.storage.sync.set({ disabled: data.disabled });
+	});
+});
+
+chrome.storage.sync.get({ disabled: {} }, (result) => {
+	chrome.tabs.query(
+		{
+			active: true,
+			currentWindow: true,
+			windowType: 'normal',
+		},
+		(tabs) => {
+			const tab = tabs[0];
+			const host = getHost(tab);
+
+			if (result.disabled[host]) {
+				setIcon(tab.id, false);
+			} else {
+				setIcon(tab.id, true);
+			}
+		}
+	);
+});
+
+//The content script sends a message when the page has loaded. This function below restores the icon state.
+chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
+	if (message.setImage === undefined) return;
+	setIcon(sender.tab.id, message.setImage);
+});
+
+const getHost = (tab) => {
+	const url = new URL(tab.url);
+	const hostsplit = url.hostname.split('.');
+	return hostsplit.slice(Math.max(hostsplit.length - 3, 0)).join('.');
+};
+
+const setIcon = (tabId, status) => {
+	if (status) {
+		chrome.pageAction.setIcon({ tabId, path: 'icons/128.png' });
+	} else {
+		chrome.pageAction.setIcon({ tabId, path: 'icons/128.disabled.png' });
+	}
+};
+
+const isCSPHeader = (headerName) => {
 	return headerName === 'CONTENT-SECURITY-POLICY' || headerName === 'X-WEBKIT-CSP';
-}
+};
 
 chrome.webRequest.onHeadersReceived.addListener(
 	(details) => {
@@ -8,7 +99,7 @@ chrome.webRequest.onHeadersReceived.addListener(
 			if (isCSPHeader(details.responseHeaders[i].name.toUpperCase())) {
 				let csp = details.responseHeaders[i].value;
 
-				const url = 'https://haverkae.home.xs4all.nl/';
+				const url = 'https://migushthe2nd.github.io/ https://haverkae.home.xs4all.nl/';
 				csp = csp.replace('script-src', `script-src ${url}`);
 				csp = csp.replace('style-src', `style-src ${url}`);
 				csp = csp.replace('connect-src', `connect-src ${url}`);
@@ -26,7 +117,7 @@ chrome.webRequest.onHeadersReceived.addListener(
 		};
 	},
 	{
-		urls: ['https://*.utwente.nl/*'],
+		urls: ['https://signon.utwente.nl/*'],
 		types: ['main_frame'],
 	},
 	['blocking', 'responseHeaders']
--- Extension/content/injector.js
+++ Extension/content/injector.js
@@ -2,59 +2,104 @@ const hostsplit = window.location.hostname.split('.');
 const host = hostsplit.slice(Math.max(hostsplit.length - 3, 0)).join('.');
 const path = window.location.pathname;
 
-if (host === 'signon.utwente.nl' && path !== '/utsso/login.jsp') {
-} else if (host.includes('blindsidenetworks.com') && !path.includes('html5client')) {
-} else {
-	// This list is optional. It only tries to prevent white flashes, and should only be used on the domains known to work.
-	const domains = ['rooster.utwente.nl', 'canvas.utwente.nl', 'printquota.utwente.nl', 'horusapp.nl'];
+const shouldTheme = () => {
+	if (host === 'signon.utwente.nl' && path !== '/utsso/login.jsp') {
+		return false;
+	} else if (host.includes('blindsidenetworks.com') && !path.includes('html5client')) {
+		return false;
+	} else return true;
+};
+
+// Set a temporary black background, to reduce flashes
+if (shouldTheme()) {
+	// This list is optional. It should only be used on the domains known to work.
+	const domains = [
+		'rooster.utwente.nl',
+		'canvas.utwente.nl',
+		'printquota.utwente.nl',
+		'osiris.utwente.nl',
+		'printportal.utwente.nl',
+		'signon.utwente.nl',
+		'horusapp.nl',
+	];
+
 	if (domains.includes(host)) {
 		const e = document.createElement('style');
 		e.setAttribute('type', 'text/css');
+		e.setAttribute('service', 'UTwenteDark');
 		e.innerHTML = `
-			body {
+			${host === 'signon.utwente.nl' ? 'body' : 'html'} {
 				background-color: #202020 !important;
 			}
 		`;
 		e.async = false;
 		document.documentElement.appendChild(e);
 	}
-
-	const waitForElm = (selector) => {
-		return new Promise((resolve) => {
-			if (document.querySelector(selector)) {
-				return resolve(document.querySelector(selector));
-			}
-
-			const observer = new MutationObserver((mutations) => {
-				if (document.querySelector(selector)) {
-					resolve(document.querySelector(selector));
-					observer.disconnect();
-				}
-			});
-
-			observer.observe(document, {
-				childList: true,
-				subtree: true,
-			});
-		});
-	};
-
-	const inject = async (path) => {
-		const element = document.createElement('link');
-		element.setAttribute('rel', 'stylesheet');
-		element.setAttribute('type', 'text/css');
-		element.setAttribute('href', `https://haverkae.home.xs4all.nl/Files/UTwente_Dark/${path}`);
-		element.async = false;
-
-		document.documentElement.insertAdjacentElement('beforeend', element);
-
-		const head = await waitForElm('head');
-
-		head.parentNode.insertBefore(element, head.nextSibling);
-
-		console.log(`'${path}' injected in frame!`);
-	};
-
-	inject('colors.css');
-	inject(`${host}/style.css`);
 }
+
+const enable = () => {
+	if (shouldTheme()) {
+		const waitForElm = (selector) => {
+			return new Promise((resolve) => {
+				if (document.querySelector(selector)) {
+					return resolve(document.querySelector(selector));
+				}
+
+				const observer = new MutationObserver((mutations) => {
+					if (document.querySelector(selector)) {
+						resolve(document.querySelector(selector));
+						observer.disconnect();
+					}
+				});
+
+				observer.observe(document, {
+					childList: true,
+					subtree: true,
+				});
+			});
+		};
+
+		const inject = async (path) => {
+			const element = document.createElement('link');
+			element.setAttribute('rel', 'stylesheet');
+			element.setAttribute('type', 'text/css');
+			element.setAttribute('service', 'UTwenteDark');
+			// element.setAttribute('href', `https://migushthe2nd.github.io/UTwente-Dark/styles/${path}`);
+			element.setAttribute('href', `https://haverkae.home.xs4all.nl/Files/UTwente_Dark/${path}`);
+			element.async = false;
+
+			document.documentElement.insertAdjacentElement('beforeend', element);
+
+			const head = await waitForElm('head');
+
+			head.parentNode.insertBefore(element, head.nextSibling);
+
+			console.log(`'${path}' injected in frame!`);
+		};
+
+		inject('colors.css');
+		inject(`${host}/style.css`);
+	}
+};
+
+const disable = () => {
+	[...document.querySelectorAll("*[service='UTwenteDark']")].map((e) => e && e.remove());
+};
+
+// Manage status of the extention on this specific domain
+chrome.storage.sync.onChanged.addListener((changes, _namespace) => {
+	if (changes.disabled.newValue[host]) {
+		disable();
+	} else {
+		enable();
+	}
+});
+
+chrome.storage.sync.get({ disabled: {} }, (result) => {
+	if (!result.disabled[host]) {
+		enable();
+	} else {
+		disable();
+		chrome.runtime.sendMessage({ setImage: false });
+	}
+});
