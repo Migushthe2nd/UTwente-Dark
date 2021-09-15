bd74d765d6f0cfa14c4c880d48014d2a23a2fe94
Migushthe2nd <ihaverkamp@live.nl>
2021-09-15 15:13:44 +0200

    Also replace media preview players

--- Extension/content/player_fixes.js
+++ Extension/content/player_fixes.js
@@ -6,3 +6,17 @@ for (const frame of videoFrames) {
 		frame.src
 	)}`;
 }
+
+const videoPreviews = document.querySelectorAll("#media_preview[data-download_url]");
+console.log(`Replacing ${videoFrames.length} preview players...`);
+
+for (const preview of videoPreviews) {
+	const frame = document.createElement("iframe");
+	frame.src = `https://migushthe2nd.github.io/UTwente-Dark/styles/canvas.utwente.nl/player.html?directurl=${encodeURIComponent(
+		preview.getAttribute("data-download_url")
+	)}&type=${preview.getAttribute("data-type")}`;
+	frame.allowFullscreen = "allowfullscreen";
+	frame.allow = "fullscreen";
+	frame.classList.add("mejs-container");
+	preview.replaceWith(frame);
+}
